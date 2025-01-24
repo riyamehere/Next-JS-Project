8d2f19518888809ee7c93ebd7e6cf8dcb4542940
Riya Mehere <riyamehere@gmail.com>
2025-01-24 23:42:50 +0530

    cache options

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -6,7 +6,9 @@ interface User {
 }
 
 const usersPage = async() => {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const res = await fetch(
+    'https://jsonplaceholder.typicode.com/users',
+    {cache: 'no-store'});
   const users: User[] = await res.json();
 
   return (
